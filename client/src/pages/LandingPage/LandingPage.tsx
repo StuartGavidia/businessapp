b7f10fa0b5249a58f492b47bb57b1a33ebfd3e47
StuartGavidia <stuarta0403@hotmail.com>
2023-10-23 23:55:56 -0700

    Rebuild Landing Page with Bootstrap

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -6,40 +6,40 @@ import FeatureData from '../../interfaces/FeatureData'
 import Price from '../../components/Price/Price'
 import PriceData from '../../interfaces/PriceData'
 import Contact from '../../components/Contact/Contact'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 
 const LandingPage:React.FC = () => {
     const features: FeatureData[] = [
         {
-            imgSource: "./assets/images/calendarImage.jpg",
             title: "Calendar",
             reasonOne: "Event Creation: Add, edit, or delete events quickly",
             reasonTwo: "Team Availability: View team schedules for easy planning.",
             reasonThree: "Public & Private Calendars: Manage team and personal calendars.",
-            alt: "Calendar"
+            icon: "bi-bar-chart-fill"
         },
         {
-            imgSource: "./assets/images/analyticsImage.png",
             title: "Analytics",
             reasonOne: "Real-Time Dashboards: Instant insights on key performance indicators.",
-            reasonTwo: "Custom Reports: Generate and share tailored analytics.",
-            reasonThree: "Data-Driven Decisions: Use data for smarter strategy and higher profits.",
-            alt: "Image of Analytics page"
+            reasonTwo: "Custom Reports: Generate tailored analytics.",
+            reasonThree: "Data-Driven Decisions: Use data for strategy and higher profits.",
+            icon: "bi-calendar-date"
         },
         {
-            imgSource: "./assets/images/invoicePicture.jpeg",
-            title: "Invoices and Payroll",
-            reasonOne: "Invoices: Generate and send bills effortlessly.",
-            reasonTwo: "Payroll: Simplify employee payments and taxes.",
-            reasonThree: "Financial Tracking: Monitor invoices and payroll in one place.",
-            alt: "Clipboard"
+          title: "Invoices and Payroll",
+          reasonOne: "Invoices: Create and send detailed bills effortlessly to streamline your billing process.",
+          reasonTwo: "Payroll: Manage employee salaries with ease, including tax computations.",
+          reasonThree: "Financial Tracking: Oversee all financial activities, from invoices to payroll, in one place.",
+          icon: "bi-chat-dots-fill"
         },
         {
-            imgSource: "./assets/images/communicationImage.jpeg",
             title: "Communication",
-            reasonOne: "Chat Rooms: Instant team messaging.",
-            reasonTwo: "File Sharing: Easy document exchange.",
-            reasonThree: "Notifications: Real-time updates and alerts.",
-            alt: "Microphone for communication"
+            reasonOne: "Chat Rooms: Foster team collaboration with dedicated messaging channels.",
+            reasonTwo: "File Sharing: Exchange and access essential documents without hassle.",
+            reasonThree: "Notifications: Stay informed with timely alerts on crucial team events.",
+            icon: "bi-wallet2"
         }
     ]
     const prices: PriceData[] = [
@@ -50,6 +50,7 @@ const LandingPage:React.FC = () => {
         {
             price: "500",
             backgroundColor: "#67C9F1"
+
         },
         {
             price: "1000",
@@ -59,48 +60,64 @@ const LandingPage:React.FC = () => {
 
     return (
         <>
-            <Header />
-            <div className="landing-intro">
-                <img src="./assets/images/peopleWorkingTogether.jpeg" alt="People working together"/>
-                <p>Unify, Simplify, Amplify: Your Team Management, All in One App.</p>
-            </div>
-            <div id="features" className="features">
-                <h2>Features</h2>
-                <div>
-                    {
-                        features.map((feature:FeatureData, i:number) => {
-                            return <Feature
-                                imgSource={feature.imgSource}
-                                title={feature.title}
-                                reasonOne={feature.reasonOne}
-                                reasonTwo={feature.reasonTwo}
-                                reasonThree={feature.reasonThree}
-                                alt={feature.alt}
-                                key={i}
-                            />
-                        })
-                    }
-                </div>
-            </div>
-            <div id="prices" className="prices">
-                <h2>Pricing</h2>
-                <div>
-                    {
-                        prices.map((currentPrice:PriceData, i:number) => {
-                            return <Price
-                                price={currentPrice.price}
-                                backgroundColor={currentPrice.backgroundColor}
-                                key={i}
-                            />
-                        })
-                    }
-                </div>
-            </div>
-            <div id="contact" className="contact">
-                <h2>Contact us</h2>
-                <Contact />
-            </div>
-            <Footer />
+          <Header />
+          <Container fluid="md" className="mt-5">
+            <Row className="d-flex align-items-center">
+              <Col className="d-flex justify-content-center">
+                <Image src="./assets/images/peopleWorkingTogether.jpeg" rounded style={{ height: 400 }} />
+              </Col>
+              <Col  className="d-flex align-items-center justify-content-start" style={{textAlign: 'center', height: 400}}>
+                <h2>Unify, Simplify, Amplify: Your Team Management, All in One App.</h2>
+              </Col>
+            </Row>
+          </Container>
+          <Container fluid="md">
+            <h2 className="text-center my-4" id="features">Features</h2>
+            <Row className="justify-content-center d-flex">
+                {
+                  features.map((feature: FeatureData, i: number) => {
+                    return (
+                      <Col xs={8} sm={6} md={5} lg={5} className="mb-5 d-flex" key={i}>
+                        <Feature
+                          title={feature.title}
+                          reasonOne={feature.reasonOne}
+                          reasonTwo={feature.reasonTwo}
+                          reasonThree={feature.reasonThree}
+                          icon={feature.icon}
+                        />
+                      </Col>
+                    )
+                  })
+                }
+            </Row>
+          </Container>
+          <Container fluid="md">
+            <h2 className="text-center mb-4" id="prices">Pricing</h2>
+            <Row className="justify-content-center align-items-center g-0">
+              {
+                prices.map((currentPrice:PriceData, i:number) => {
+                  return (
+                    <Col xs={8}
+                      sm={8}
+                      md={3}
+                      lg={3}
+                      className="mb-5 d-flex" key={i}>
+                      <Price
+                        price={currentPrice.price}
+                        backgroundColor={currentPrice.backgroundColor}
+                        key={i}
+                      />
+                    </Col>
+                  )
+                })
+              }
+            </Row>
+          </Container>
+          <Container fluid="md">
+            <h2 className="text-center" id="contact">Contact us</h2>
+            <Contact />
+          </Container>
+          <Footer />
         </>
     )
 }
