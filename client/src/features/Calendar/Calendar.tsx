fb4eb0f6be152de61b051bac35cb9d5ba302b2eb
StuartGavidia <stuarta0403@hotmail.com>
2023-11-04 20:24:37 -0700

    Track form data and prepare for request

--- client/src/features/Calendar/Calendar.tsx
+++ client/src/features/Calendar/Calendar.tsx
@@ -5,32 +5,64 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Select from 'react-select';
 import { useState, useEffect } from 'react'
 import UserServiceAPI from '../../api/userServiceAPI'
+import { MultiValue } from 'react-select';
+
+interface EventStructure {
+  action: string,
+  type: string,
+}
+
+interface Employee {
+  userId: string;
+  firstName: string,
+  lastName: string,
+  picture?: string,
+  status?: string;
+  label?: string;
+}
+
+interface EventData {
+  title: string,
+  location: string,
+  description: string,
+  startTime: string, //YYYY-MM-DDTHH:MM
+  endTime: string, //YYYY-MM-DDTHH:MM,
+  eventAttendees: Employee[],
+  status: string
+}
+
 
 const Calendar:React.FC = () => {
 
   const [showEventModal, setShowEventModal] = useState(false)
-  const [showMeetingModal, setShowMeetingModal] = useState(false)
+  const [eventStructure, setEventStructure] = useState<EventStructure>({ action: '', type: '' });
+  const [eventData, setEventData] = useState<EventData>({
+    title: '',
+    location: '',
+    description: '',
+    startTime: '',
+    endTime: '',
+    eventAttendees: [],
+    status: ''
+  })
 
-  const [modalAction, setModalAction] = useState("")
+  const handleModalSubmit = () => {
+    console.log(eventData)
+    toggleEventModal()
+  }
 
   const toggleEventModal = () => {
     setShowEventModal((prev: boolean) => !prev)
   }
 
-  const toggleMeetingModal = () => {
-    setShowMeetingModal((prev: boolean) => !prev)
-  }
-
-  const handleCreateMeeting = () => {
-    toggleMeetingModal()
-  };
-
   const handleEventClick = (info:any) => {
-    //TODO: we need to add a check to see if it is a meeting event(as that will contain different data)
-    //after confirming it is a meeting event, then toggle that modal instead
-
     //this code prevents url navigations
     info.jsEvent.preventDefault();
     if (info.event.url) {
@@ -38,16 +70,20 @@ const Calendar:React.FC = () => {
     }
 
     console.log(info)
-    setModalAction("Edit")
+    setEventStructure(prev => ({ ...prev, action: 'Edit', type: 'Event' }));
     toggleEventModal()
   }
 
   const handleSelect = (selectionInfo:any) => {
     console.log(selectionInfo)
-    setModalAction("Add")
+    setEventStructure(prev => ({ ...prev, action: 'Add', type: 'Event' }));
     toggleEventModal()
-
   }
+
+  const handleCreateMeeting = () => {
+    setEventStructure(prev => ({ ...prev, action: 'Create', type: 'Meeting Event' }));
+    toggleEventModal()
+  };
 
   const handleEventMouseEnter = (info: any) => {
     info.el.style.borderColor = 'blue';
@@ -73,6 +109,65 @@ const Calendar:React.FC = () => {
     };
     fetchData();
   }, [])
+
+  const handleEventChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+      const { name, value } = event.target;
+      setEventData(prevData => ({
+        ...prevData,
+        [name]: value
+      }));
+  }
+
+  const handleAttendeesEventChange = (
+    event: MultiValue<Employee>
+  ) => {
+    const selectedOptions = event as MultiValue<Employee>;
+    const attendees: Employee[] = selectedOptions.map(event => {
+      return {userId: event.userId, firstName: event.firstName, lastName: event.lastName, status: "Pending", label: event.label, picture: event.picture}
+    })
+      setEventData(prevData => ({
+        ...prevData,
+        eventAttendees: attendees
+      }));
+  }
+
+  const formatOptionLabel = ({ label, picture }: Employee) => (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Image src={picture} alt={label} style={{ marginRight: '10px', width: '30px', height: '30px', borderRadius: '50%' }} />
+      <div>{label}</div>
+    </div>
+  );
+
+  const employees: Employee[] = [
+    {
+      userId: "1",
+      firstName: "Joe",
+      lastName: "John",
+      picture: "../assets/images/defaultProfilePicture.jpeg"
+    },
+    {
+      userId: "2",
+      firstName: "Jimmy",
+      lastName: "John",
+      picture: "../assets/images/defaultProfilePicture.png"
+    },
+    {
+      userId: "3",
+      firstName: "Lebron",
+      lastName: "James",
+      picture: "../assets/images/defaultProfilePicture.png"
+    }
+  ]
+
+  const employeeOptions: Employee[] = employees.map(employee => ({
+    userId: employee.userId,
+    firstName: employee.firstName,
+    lastName: employee.lastName,
+    label: `${employee.firstName} ${employee.lastName}`,
+    picture: employee.picture
+  }));
 
   return (
     <>
@@ -105,28 +200,101 @@ const Calendar:React.FC = () => {
       />
       <Modal show={showEventModal} onHide={toggleEventModal}>
         <Modal.Header closeButton>
-          <Modal.Title>{modalAction} Event</Modal.Title>
+          <Modal.Title>{eventStructure.action} {eventStructure.type}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>The form will go here that contains event data</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Row className="mb-3">
+              <Col md={6}>
+                <Form.Group controlId="title">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Title"
+                    autoFocus
+                    name="title"
+                    onChange={handleEventChange}
+                    required
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="location">
+                  <Form.Label>Location</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Location"
+                    autoFocus
+                    name="location"
+                    onChange={handleEventChange}
+                    required
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group
+              className="mb-3"
+              controlId="description"
+            >
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                name="description"
+                onChange={handleEventChange}
+                required
+              />
+            </Form.Group>
+            <Row className="mb-3">
+              <Col md={6}>
+                <Form.Group controlId="startTime">
+                  <Form.Label>Start Time</Form.Label>
+                  <Form.Control
+                    type="datetime-local"
+                    autoFocus
+                    name="startTime"
+                    onChange={handleEventChange}
+                    required
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="endTime">
+                  <Form.Label>End Time</Form.Label>
+                  <Form.Control
+                    type="datetime-local"
+                    name="endTime"
+                    onChange={handleEventChange}
+                    required
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group as={Row} className="mb-3" controlId="eventAttendees">
+              <Form.Label column sm={2}>
+                Attendees
+              </Form.Label>
+              <Col sm={10}>
+                <Select
+                  isMulti
+                  name="attendees"
+                  options={employeeOptions}
+                  className="basic-multi-select"
+                  classNamePrefix="select"
+                  formatOptionLabel={formatOptionLabel}
+                  getOptionValue={option => option.userId}
+                  onChange={handleAttendeesEventChange}
+                  value={eventData.eventAttendees}
+                />
+              </Col>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={toggleEventModal}>
             Close
           </Button>
-          <Button variant="primary" onClick={toggleEventModal}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <Modal show={showMeetingModal} onHide={toggleMeetingModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>{modalAction} Meeting</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>The form will contain meeting form fields</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={toggleMeetingModal}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={toggleMeetingModal}>
+          <Button variant="primary" onClick={handleModalSubmit}>
             Save Changes
           </Button>
         </Modal.Footer>
